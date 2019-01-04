f7aa8cb9ca587a86ad4a60b2886884006982cb7b
JonesTPG <joonas.ryynanen1@gmail.com>
2019-01-04 14:56:47 +0200

    courses.jsx and server.js

---
+++ src/components/courses.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+const axios = require("axios");
+
+class Courses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: null
+    };
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+  }
+  getDataFromDb = () => {
+    axios.get("/api/all").then(response => {
+      console.log(response.data);
+      var data = JSON.parse(response.data);
+      console.log(data);
+    });
+  };
+
+  render() {
+    if (this.state.courses === null) {
+      return "loading";
+    }
+
+    return (
+      <React.Fragment>
+        {" "}
+        <h1>Test</h1>
+        <ul>
+          {this.state.courses.map(course => (
+            <li
+              key={course._id}
+              //className="course-list-item"
+              //onClick={() => this.setSelectedcourse(course)}
+            >
+              {course.title}
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Courses;
+
