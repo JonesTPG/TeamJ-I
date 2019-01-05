b9d1d708058a1052cd77c1ff0015989861a51452
JanettaH <janetta.huoponen@gmail.com>
2019-01-05 13:48:30 +0200

    added AppBar, Toolbar and TextField

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Toolbar from "./components/toolbar";
 import Courses from "./components/courses";
-
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toolbar />
         <Courses />
-        
       </div>
     );
   }
