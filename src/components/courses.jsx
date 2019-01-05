22cc7b3b58b9e3ac07a433b49e39face23630725
JonesTPG <joonas.ryynanen1@gmail.com>
2019-01-05 14:56:43 +0200

    courses component list styles

--- src/components/courses.jsx
+++ src/components/courses.jsx
@@ -3,87 +3,124 @@ import SelectedCourse from "./selectedcourse";
 import TextField from "@material-ui/core/TextField";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import Divider from "@material-ui/core/Divider";
 const axios = require("axios");
 
+const styles = theme => ({
+ root: {
+   width: "100%",
+   maxWidth: 650,
+   backgroundColor: theme.palette.background.paper
+ },
+ Checkbox: {}
+});
+
 class Courses extends Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+   super(props);
 
-    this.state = {
-      courses: null,
-      filter: "",
-      selectedCourse: null,
-      course_id: null //stores the currently selected course id
-    };
-    this.setFilter = this.setFilter.bind(this);
-    this.setSelectedCourse = this.setSelectedCourse.bind(this);
-  }
+   this.state = {
+     checked: true,
+     courses: null,
+     filter: "",
+     selectedCourse: null,
+     course_id: null //stores the currently selected course id
+   };
+   this.setFilter = this.setFilter.bind(this);
+   this.setSelectedCourse = this.setSelectedCourse.bind(this);
+ }
 
-  // fetch data from mlab database, add to courses
-  componentDidMount() {
-    this.getDataFromDb();
-  }
+ // fetch data from mlab database, add to courses
+ componentDidMount() {
+   this.getDataFromDb();
+ }
 
-  getDataFromDb = () => {
-    axios.get("/api/all").then(response => {
-      console.log(response.data);
-      var data = JSON.parse(response.data);
-      this.setState({
-        courses: data
-      });
-    });
-  };
+ getDataFromDb = () => {
+   axios.get("/api/all").then(response => {
+     console.log(response.data);
+     var data = JSON.parse(response.data);
+     this.setState({
+       courses: data
+     });
+   });
+ };
 
-  setFilter(e) {
-    this.setState({ filter: e.target.value });
-  }
+ handleToggle = () => {
+   this.setState({
+     checked: !this.state.checked
+   });
+ };
 
-  setSelectedCourse(course) {
-    this.setState({ course_id: course });
-  }
+ setFilter(e) {
+   this.setState({ filter: e.target.value });
+ }
 
-  render() {
-    if (this.state.courses === null) {
-      return "loading";
-    }
+ setSelectedCourse(course) {
+   this.setState({ course_id: course });
+ }
 
-    const filtered = this.state.courses.filter(
-      course =>
-        course.coursename
-          .toLowerCase()
-          .indexOf(this.state.filter.toLowerCase()) !== -1
-    );
+ render() {
+   const { classes } = this.props;
 
-    return (
-      <React.Fragment>
-        {" "}
-        <div className="course-container">
-          <AppBar position="static" color="primary">
-            <Toolbar>
-              <TextField
-                value={this.state.filter}
-                onChange={this.setFilter}
-                label="Etsi..."
-              />
-            </Toolbar>
-          </AppBar>
-          <ul>
-            {filtered.map(course => (
-              <li
-                key={course._id}
-                //className="course-list-item"
-                onClick={() => this.setSelectedCourse(course._id)}
-              >
-                {course.courseid} {course.coursename}
-              </li>
-            ))}
-          </ul>
+   if (this.state.courses === null) {
+     return "loading";
+   }
 
-          <SelectedCourse courseid={this.state.course_id} />
-        </div>
-      </React.Fragment>
-    );
-  }
+   const filtered = this.state.courses.filter(
+     course =>
+       course.coursename
+         .toLowerCase()
+         .indexOf(this.state.filter.toLowerCase()) !== -1
+   );
+
+   return (
+     <React.Fragment>
+       {" "}
+       <div className="course-container">
+         <AppBar position="static" color="primary">
+           <Toolbar>
+             <TextField
+               value={this.state.filter}
+               onChange={this.setFilter}
+               label="Etsi..."
+             />
+           </Toolbar>
+         </AppBar>
+         <List className={classes.root}>
+           {filtered.map(course => (
+             <div>
+               <ListItem
+                 key={course._id}
+                 onClick={() => this.setSelectedCourse(course._id)}
+               >
+                 {course.courseid} {course.coursename}
+                 <ListItemSecondaryAction>
+                   <Checkbox
+                     checked={this.state.isChecked}
+                     onChange={this.handleToggle}
+                   />
+                 </ListItemSecondaryAction>
+               </ListItem>
+               <Divider light />
+             </div>
+           ))}
+         </List>
+         <SelectedCourse courseid={this.state.course_id} />
+       </div>
+     </React.Fragment>
+   );
+ }
 }
+Courses.propTypes = {
+ classes: PropTypes.object.isRequired
+};
 
-export default Courses;
+export default withStyles(styles)(Courses);
+
